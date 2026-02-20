8d93eeecaffc243b8f3385b18be6deef0c3309c8
mollesskin <moldiromar42@gmail.com>
2026-02-20 19:47:36 +0500

    Delete test_samples directory

--- test_samples/check_me.js
+++
@@ -1,9 +0,0 @@
-const a = Number(req.body.a);
-const b = Number(req.body.b);
-
-if (!Number.isFinite(a) || !Number.isFinite(b)) {
-  return res.status(400).send("Invalid input");
-}
-
-const result = a + b;
-res.send(String(result));
